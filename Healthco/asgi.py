d5a248f1cb1468ad5c0a749abd384bda825bd7c1
Wassim Maziane <mazianewassim@gmail.com>
2023-11-24 21:55:59 +0100

    fixed prototype frontend

--- Healthco/asgi.py
+++ Healthco/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HMS project.
+ASGI config for Healthco project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HMS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healthco.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HMS.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healthco.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
